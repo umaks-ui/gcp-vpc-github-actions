53fcb438cd3f3e7e42d770a95bf03ce5bbd2f35a
umaks-ui <uma.ks@enhub.ai>
2026-07-28 22:27:29 +0530

    Fix Terraform formatting in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 resource "google_compute_network" "custom_vpc" {
   name                    = var.vpc_name
-  auto_create_subnetworks = false # Set to false for Custom Subnet Mode
-  routing_mode            = var.routing_mode # REGIONAL or GLOBAL
-  mtu                     = var.mtu # e.g., 1460 or 1500
+  auto_create_subnetworks = false # Custom subnet mode
+  routing_mode            = var.routing_mode
+  mtu                     = var.mtu
   project                 = var.project_id
 }
